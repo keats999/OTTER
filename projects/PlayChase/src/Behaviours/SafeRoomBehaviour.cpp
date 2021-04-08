25007d8b081663042032c43573ca46d823916428
keats999 <keaton.plegde@ontariotechu.net>
2021-04-08 13:49:12 -0400

    pause fix

--- projects/PlayChase/src/Behaviours/SafeRoomBehaviour.cpp
+++ projects/PlayChase/src/Behaviours/SafeRoomBehaviour.cpp
@@ -1,28 +1,32 @@
 #include "SafeRoomBehaviour.h"
+#include "Application.h"
+#include "Utilities/Globals.h"
 
 void SafeRoomBehaviour::Update(entt::handle entity)
 {
-	float dt = Timing::Instance().DeltaTime;
-	glm::vec3 pos = entity.get<Transform>().GetLocalPosition();
-	if (locked) {
-		entity.get<Transform>().SetLocalPosition(glm::vec3(pos.x, 0.0, pos.z));
-		locktimer += dt;
-		if (locktimer > 5.0f) {
-			locktimer = 0.0f;
-			MapManager::Instance().set_data(roomi, roomj, true);
-			locked = false;
-			ready = false;
+	if (Application::Instance().ActiveScene == Globals::Instance().scenes[1])
+	{
+		float dt = Timing::Instance().DeltaTime;
+		glm::vec3 pos = entity.get<Transform>().GetLocalPosition();
+		if (locked) {
+			entity.get<Transform>().SetLocalPosition(glm::vec3(pos.x, 0.0, pos.z));
+			locktimer += dt;
+			if (locktimer > 5.0f) {
+				locktimer = 0.0f;
+				MapManager::Instance().set_data(roomi, roomj, true);
+				locked = false;
+				ready = false;
+			}
+		}
+		else {
+			entity.get<Transform>().SetLocalPosition(glm::vec3(pos.x, 1.5, pos.z));
+		}
+		if (!ready) {
+			cdtimer += dt;
+			if (cdtimer > 5.0f) {
+				cdtimer = 0.0f;
+				ready = true;
+			}
 		}
 	}
-	else {
-		entity.get<Transform>().SetLocalPosition(glm::vec3(pos.x, 1.5, pos.z));
-	}
-	if (!ready) {
-		cdtimer += dt;
-		if (cdtimer > 5.0f) {
-			cdtimer = 0.0f;
-			ready = true;
-		}
-	}
-	
 }
